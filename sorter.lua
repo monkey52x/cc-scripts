0a5ebdaea4585ee2c89711041c4e47b6f1f08512
MONKEY 52 <monkey52.team@gmail.com>
2025-09-05 10:29:47 +0200

    Update sorter.lua

--- sorter.lua
+++ sorter.lua
@@ -1,16 +1,39 @@
-local input = peripheral.wrap("right")       -- буферный сундук
-local sword = peripheral.wrap("left")        -- сундук для мечей
-local other = peripheral.wrap("bottom")      -- сундук для остальных
+-- читаем список тегов из файла
+local swordTags = {}
+if fs.exists("tags.txt") then
+  local f = fs.open("tags.txt", "r")
+  while true do
+    local line = f.readLine()
+    if not line then break end
+    table.insert(swordTags, line)
+  end
+  f.close()
+else
+  print("Файл tags.txt не найден!")
+end
+
+local input = peripheral.wrap("right")
+local sword = peripheral.wrap("left")
+local other = peripheral.wrap("bottom")
+
+local function isSword(itemName)
+  local name = itemName:lower()
+  for _, tag in ipairs(swordTags) do
+    if string.find(name, tag) then
+      return true
+    end
+  end
+  return false
+end
 
 while true do
   for slot=1, input.size() do
     local item = input.getItemDetail(slot)
     if item then
-      local nm = (item.name or ""):lower()
-      if string.find(nm, "sword") then
-        input.pushItems("left", slot)   -- если сундук слева
+      if isSword(item.name) then
+        input.pushItems("left", slot)
       else
-        input.pushItems("bottom", slot) -- если сундук снизу
+        input.pushItems("bottom", slot)
       end
     end
   end
