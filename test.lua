342329609f31323158696480446ded4e6e46c50b
MONKEY 52 <monkey52.team@gmail.com>
2025-09-05 12:22:59 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,16 +1,27 @@
-
-local chest = peripheral.wrap("left")
-
-function transferAll()
-    for slot = 1, 16 do
-        local item = commands.getItemDetail(slot)
-        if item then
-            chest.pullItems(peripheral.getName(peripheral.find("inventory")), slot)
-        end
+-- Функция для добавления предмета в слот с проверкой
+function acceptItem(slot)
+    local item = turtle.getItemDetail(slot)
+    if item and item.name == "minecraft:dirt" then
+        return true  -- принимаем только землю
+    else
+        return false -- игнорируем все остальное
     end
 end
 
+-- Основной цикл
 while true do
-    transferAll()
+    for slot = 1, 16 do
+        local item = turtle.getItemDetail(slot)
+        if item then
+            if acceptItem(slot) then
+                -- Можно, например, положить в сундук слева
+                turtle.select(slot)
+                turtle.drop()  -- выбрасываем в сундук слева
+            else
+                print("Предмет не разрешен: "..item.name)
+                -- можно выбросить в пустоту или оставить в слоте
+            end
+        end
+    end
     sleep(0.5)
 end
