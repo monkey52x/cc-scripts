bbab2156ef7d456b7463c8269248e7f3c7459e59
MONKEY 52 <monkey52.team@gmail.com>
2025-09-05 12:28:10 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,27 +1,18 @@
--- Функция для добавления предмета в слот с проверкой
-function acceptItem(slot)
-    local item = turtle.getItemDetail(slot)
-    if item and item.name == "minecraft:dirt" then
-        return true  -- принимаем только землю
-    else
-        return false -- игнорируем все остальное
-    end
-end
+local chest = peripheral.wrap("left")  -- сундук слева
 
--- Основной цикл
 while true do
     for slot = 1, 16 do
         local item = turtle.getItemDetail(slot)
         if item then
-            if acceptItem(slot) then
-                -- Можно, например, положить в сундук слева
+            if item.name == "minecraft:dirt" then
                 turtle.select(slot)
-                turtle.drop()  -- выбрасываем в сундук слева
+                turtle.drop()  -- отправляем землю в сундук
             else
-                print("Предмет не разрешен: "..item.name)
-                -- можно выбросить в пустоту или оставить в слоте
+                -- Любой другой предмет сразу возвращаем в сундук
+                turtle.select(slot)
+                turtle.drop(64)  -- сбрасываем в сундук слева
             end
         end
     end
-    sleep(0.5)
+    sleep(0.2)  -- проверяем часто, чтобы трубами ничего не задержалось
 end
